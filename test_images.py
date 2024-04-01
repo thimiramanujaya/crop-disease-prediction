9ecda9f77e944e1af9174fabe93bb5ce5c50441a
thimiramanujaya <128280023+thimiramanujaya@users.noreply.github.com>
2024-04-01 14:04:48 +0530

    inserted test_images.py & sample imgs for testing
    1. Inserted test_images.py for defining images paths and names
    2. Inserted a new directory named 'assets' that contains sample images for testing models

---
+++ test_images.py
@@ -0,0 +1,37 @@
+images_info = {
+    'rice': [
+        {'path': './assets/rice_bacterial_blight.jpg', 'name': 'Bacterial Blight'},
+        {'path': './assets/rice_blast.jpg', 'name': 'Blast'},
+        {'path': './assets/rice_brownspot.jpg', 'name': 'Brownspot'},
+        {'path': './assets/rice_tungro.jpeg', 'name': 'Tungro'},
+    ],
+
+    'potato': [
+        {'path': './assets/potato_early_blight.jpg', 'name': 'Early Blight'},
+        {'path': './assets/potato_healthy_leaf.jpg', 'name': 'Healthy Leaf'},
+        {'path': './assets/potato_late_blight.jpg', 'name': 'Late Blight'},
+    ],
+
+    'tomato': [
+        {'path': './assets/tomato_bacterial_spot.jpg', 'name': 'Bacterial Spot'},
+        {'path': './assets/tomato_early_blight.jpg', 'name': 'Early Blight'},
+        {'path': './assets/tomato_late_blight.jpg', 'name': 'Late Blight'},
+        {'path': './assets/tomato_leaf_mold.jpg', 'name': 'Leaf Mold'},
+        {'path': './assets/tomato_mosaic_virus.jpg', 'name': 'Mosaic Virus'},
+        {'path': './assets/tomato_septoria_leaf_spot.jpg', 'name': 'Septoria Leaf Spot'},
+        {'path': './assets/tomato_spider_mites_two_spotted_spider_mite.jpg', 'name': 'Spider Mites Two Spotted Spider Mite'},
+        {'path': './assets/tomato_target_spot.jpg', 'name': 'Target Spot'},
+        {'path': './assets/tomato_yellowleaf_curl_virus.jpg', 'name': 'Yellowleaf Curl Virus'},
+        {'path': './assets/tomato_healthy_leaf.jpg', 'name': 'Healthy Leaf'},
+    ],
+    'bellpepper': [
+        {'path': './assets/bellpepper_bacterial_spot_1.jpg', 'name': 'Bacterial_spot'},
+        {'path': './assets/bellpepper_healthy_leaf.jpg', 'name': 'Healthy Leaf'},
+        {'path': './assets/bellpepper_bacterial_spot_2.jpg', 'name': 'Bacterial Spot'},
+    ],
+    'cinnamon': [
+        {'path': './assets/cinnamon_leaf_spot_desease_2.jpg', 'name': 'Leaf Spot Desease'},
+        {'path': './assets/cinnamon_healthy_leaf.jpg', 'name': 'Healthy Leaf'},
+        {'path': './assets/cinnamon_leaf_spot_desease_1.jpg', 'name': 'Leaf Spot Desease'},
+    ],
+}
